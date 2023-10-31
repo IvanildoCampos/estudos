3af7a3e7f7a1be4ff9e96bd4ca8fad3ecb089ff2
Ivanildo Campos <125228040+IvanildoCampos@users.noreply.github.com>
2023-10-30 23:10:13 -0300

    Initial commit

---
+++ Projeto-Java/projetoJava/src/repetical/App.java
@@ -0,0 +1,17 @@
+/**public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+*/
+package repetical
+
+public class repetical {
+    public static void main (String [] args) {
+        int cc = 0
+        do {
+            System.out.println(cambalhota);
+            cc++;
+        } while (cc<4);
+    }
+}
---
+++ projeto-bateria/README.md
@@ -0,0 +1,2 @@
+# projeto-bateria
+ projeto criado com B7web
